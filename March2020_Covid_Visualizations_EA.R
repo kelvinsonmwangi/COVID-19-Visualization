76d00e6c20e914f061820d2176350eabffca8030
Kevinson Mwangi <knmwangi4@gmail.com>
2020-03-31 22:45:04 +0300

    Update March2020_Covid_Visualizations_EA.R

--- March2020_Covid_Visualizations_EA.R
+++ March2020_Covid_Visualizations_EA.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(gganimate)
+library(gifski)
+library(rtweet)
 
 coviddata <-read_csv("https://covid.ourworldindata.org/data/ecdc/total_cases.csv")
 
@@ -9,13 +11,21 @@ easternafrica <- coviddata %>%
 
 easternafrica <- easternafrica[!is.na(easternafrica$cases), ] 
 
-easternafrica %>% 
+plot <- easternafrica %>% 
 ggplot() +
   geom_line(aes( x = date, y = cases, color = country), size = 1) +
   labs(title = "Trends in COVID-19 cases in Eastern Africa", 
        subtitle = "Data Source: https://ourworldindata.org/coronavirus-source-data", 
-       caption = "Code: https://https://github.com/kelvinsonmwangi/COVID-19-Visualization") +
+       caption = "Code://https://github.com/kelvinsonmwangi/COVID-19-Visualization") +
   xlab("")+
   ylab("Confirmed cases") +
   theme_test(base_size = 14) +
   transition_reveal(date)
+
+animate(plot, nframes = 150, renderer = gifski_renderer("COVID-19EA.gif"))
+
+#Spread the news
+post_tweet(
+  status = "Visualizing changes in COVID-19 confirmed cases across Eastern Africa using #rstats ",
+  media = 'COVID-19EA.gif'
+)
